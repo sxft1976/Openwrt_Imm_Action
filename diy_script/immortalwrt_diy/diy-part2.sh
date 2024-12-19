189bd6cf40afee108c7b799dec3a256997bb5e96
sxft1976 <hanlif@hotmail.com>
2024-12-19 10:30:15 +0800

    Initial commit

---
+++ diy_script/immortalwrt_diy/diy-part2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2023 SmallProgram <https://github.com/smallprogram>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/smallprogram/OpenWrtAction
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+
+# rollback ruby verion from 3.3.4 to 3.2.2
+# cd feeds/packages
+# git fetch --unshallow
+# git checkout 565e79e73619f806bc56ef189917ba013f306023 -- lang/ruby
+# git checkout HEAD -- lang/ruby
+# cd ..
+# cd ..
+
+
+# Modify default passwd
+# sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
+
+# Temporary Fix
+# sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
+
+# Reset drive type
+# sed -i 's/(dmesg | grep .*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+# sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
+# sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
+
+# Close running yards
+# sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
+
+# Diy
+# rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
+
+echo "DIY2 is complate!"
