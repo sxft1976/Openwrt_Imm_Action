dbcab2fcf19f384da644b741184d80a5d85a56dd
sxft1976 <hanlif@hotmail.com>
2024-12-25 20:05:50 +0800

    Update diy-part1.sh

--- diy_script/tag_immortalwrt_diy/diy-part1.sh
+++ diy_script/tag_immortalwrt_diy/diy-part1.sh
@@ -31,22 +31,10 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custo
 # rm -rf ./package/lean/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
 
-# homeproxy incloude luci pakcage
-# git clone https://github.com/immortalwrt/homeproxy.git ./package/custom_packages/homeproxy
 
-# mosdns
 
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
-rm -rf ./feeds/luci/applications/luci-app-mosdns/
-rm -rf ./feeds/packages/net/mosdns/
-# rm -rf feeds/packages/net/v2ray-geodata/
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata ./package/custom_packages/v2ray-geodata
-
-# git clone https://github.com/jerrykuku/lua-maxminddb.git
-# git clone https://github.com/jerrykuku/luci-app-vssr.git
-
 
 # Open App Filter
 git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/luci-app-openappfilter
