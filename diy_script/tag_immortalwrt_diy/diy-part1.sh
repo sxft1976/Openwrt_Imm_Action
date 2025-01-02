01636688b7045d6860780ca909121519a6c32778
sxft1976 <hanlif@hotmail.com>
2025-01-02 10:56:48 +0800

    Update diy-part1.sh

--- diy_script/tag_immortalwrt_diy/diy-part1.sh
+++ diy_script/tag_immortalwrt_diy/diy-part1.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #
 # Copyright (c) 2019-2023 SmallProgram <https://github.com/smallprogram>
@@ -10,40 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-rm -rf ./package/custom_packages
-mkdir -p ./package/custom_packages
-
-# Add Theme
-rm -rf ./feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
-
-rm -rf ./feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
-
-# adguardhome
-# rm -rf ./package/lean/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
-
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-# Open App Filter
-git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/luci-app-openappfilter
-
-# Speedtest
-git clone https://github.com/sirpdboy/netspeedtest.git ./package/custom_packages/netspeedtest
-
-
-# delete packages
-rm -rf ./feeds/packages/net/geoview
-
-
-# smartdns
-# git clone https://github.com/pymumu/smartdns.git ./package/custom_packages/smartdns
