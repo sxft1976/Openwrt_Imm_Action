f01bb071063231849e71ef39fae27363ec24bdcd
sxft1976 <hanlif@hotmail.com>
2025-01-07 20:54:54 +0800

    Update diy-part1.sh

--- diy_script/immortalwrt_diy/diy-part1.sh
+++ diy_script/immortalwrt_diy/diy-part1.sh
@@ -14,23 +14,3 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# adguardhome
-# rm -rf ./package/lean/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
-
-
-
-
-# Open App Filter
-git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/luci-app-openappfilter
-
-# Speedtest
-git clone https://github.com/sirpdboy/netspeedtest.git ./package/custom_packages/netspeedtest
-
-
-# delete packages
-rm -rf ./feeds/packages/net/geoview
-
-
-# smartdns
-# git clone https://github.com/pymumu/smartdns.git ./package/custom_packages/smartdns
