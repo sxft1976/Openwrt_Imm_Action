b300b485d25fd93a39f5c9bc56d557a543108a15
sxft1976 <hanlif@hotmail.com>
2025-08-29 19:03:49 +0800

    Update diy-part2.sh

--- diy_script/tag_immortalwrt_diy/diy-part2.sh
+++ diy_script/tag_immortalwrt_diy/diy-part2.sh
@@ -14,7 +14,7 @@
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
 
 # update packages version
