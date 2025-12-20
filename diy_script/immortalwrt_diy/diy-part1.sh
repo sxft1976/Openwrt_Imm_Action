9fb2ef6b45f543576f4fb098e9132eed1efb29e7
sxft1976 <hanlif@hotmail.com>
2025-12-20 17:36:03 +0800

    Update diy-part1.sh

--- diy_script/immortalwrt_diy/diy-part1.sh
+++ diy_script/immortalwrt_diy/diy-part1.sh
@@ -10,43 +10,23 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/../custom_feeds_and_packages.sh"
+ rm -rf ./package/custom_packages
+    mkdir -p ./package/custom_packages
 
-# ---------------------------------------------------------------feeds update---------------------------------------------------------------
-# 备份原始 feeds.conf.default
-cp feeds.conf.default feeds.conf.default.bak
+    git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
+    git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
+    git clone https://github.com/sirpdboy/luci-theme-kucat.git ./package/custom_packages/luci-theme-kucat
+    git clone https://github.com/sirpdboy/luci-app-kucat-config.git ./package/custom_packages/luci-app-kucat-config
+    git clone https://github.com/eamonxg/luci-theme-aurora.git ./package/custom_packages/luci-theme-aurora
+    git clone https://github.com/derisamedia/luci-theme-alpha.git ./package/custom_packages/luci-theme-alpha
+    git clone https://github.com/animegasan/luci-app-alpha-config.git ./package/custom_packages/luci-app-alpha-config
+    git clone https://github.com/AngelaCooljx/luci-theme-material3.git ./package/custom_packages/luci-theme-material3
+    git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
+    git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
+    git clone https://github.com/sirpdboy/luci-app-netspeedtest ./package/custom_packages/netspeedtest
+    git clone https://github.com/timsaya/openwrt-bandix.git ./package/custom_packages/openwrt-bandix
+    git clone https://github.com/timsaya/luci-app-bandix.git ./package/custom_packages/luci-app-bandix
+    git clone https://github.com/destan19/OpenAppFilter.git ./package/custom_packages/OpenAppFilter
 
-# 删除 feeds.conf.default 中的注释行（以 # 开头）
-sed -i '/^#/d' feeds.conf.default
-sed -i -e 's|git.openwrt.org/feed|github.com/openwrt|g' -e 's|git.openwrt.org/project|github.com/openwrt|g' feeds.conf.default
 
-
-# 创建临时文件
-temp_file=$(mktemp)
-
-# 倒序遍历 repos 数组
-for ((i=${#repos[@]}-1; i>=0; i--)); do
-  repo="${repos[$i]}"
-  # 提取 feed 名称（src-git 后的第一个字段）
-  feed_name=$(echo "$repo" | awk '{print $2}')
-  
-  # 检查 feeds.conf.default 是否包含该 feed 名称
-  if ! grep -q "src-git $feed_name " feeds.conf.default; then
-    # 如果不存在，将该 repo 插入到临时文件的第一行
-    echo "$repo" > "$temp_file"
-    cat feeds.conf.default >> "$temp_file"
-    mv "$temp_file" feeds.conf.default
-    echo "Added: $repo"
-  else
-    echo "Skipped (already exists): $repo"
-  fi
-done
-
-# 清理临时文件（如果仍然存在）
-[ -f "$temp_file" ] && rm "$temp_file"
-
-echo "Updated feeds.conf.default"
-# ---------------------------------------------------------------end feeds update---------------------------------------------------------------
-
-clone_custom_packages
+    sed -i '/^[\t ]*PKG_VERSION:=/ s/\(PKG_VERSION:= *\)[^0-9.]*\([0-9.]*\)[^0-9.]*/\1\2/' "./package/custom_packages/luci-theme-alpha/Makefile"
