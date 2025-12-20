1e862495ac4c21f2a93f80dd2d5968a1171cbe9e
sxft1976 <hanlif@hotmail.com>
2025-12-20 16:25:01 +0800

    Update diy-part2.sh

--- diy_script/immortalwrt_diy/diy-part2.sh
+++ diy_script/immortalwrt_diy/diy-part2.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # Copyright (c) 2019-2025 SmallProgram <https://github.com/smallprogram>
@@ -19,14 +20,20 @@ sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generat
 # update packages version
 rm -rf temp_resp
 git clone -b master --single-branch https://github.com/openwrt/packages.git temp_resp
-cd temp_resp
-git checkout 2b99cd7d7637da0f152da378994f699aaf0dd44d
-cd ..
+# cd temp_resp
+# git checkout 2b99cd7d7637da0f152da378994f699aaf0dd44d
+# cd ..
 rm -rf feeds/packages/lang/golang
 cp -r temp_resp/lang/golang feeds/packages/lang
 rm -rf feeds/packages/lang/rust
 cp -r temp_resp/lang/rust feeds/packages/lang
 rm -rf temp_resp
+
+rm -rf temp_resp
+git clone -b main --single-branch https://github.com/openwrt/openwrt.git temp_resp
+cp -f temp_resp/scripts/patch-kernel.sh scripts/
+rm -rf temp_resp
+
 
 # rm appfilter
 rm -rf ./feeds/packages/net/open-app-filter
@@ -50,6 +57,9 @@ fi
 # rpcsvc-proto: fix build with autotools gettext macros 0.22
 # cp -r "$PATCHES_SRC_DIR/patches/rpcsvc-proto/*" ./feeds/packages/libs/rpcsvc-proto
 
+# inject download package
+mkdir -p dl
+cp -r $PATCHES_SRC_DIR/library/* ./dl/
 
 
 
